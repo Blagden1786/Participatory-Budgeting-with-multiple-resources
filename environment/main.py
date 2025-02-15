fb7e49e5098204e08cc4abdc8a3d650f1b3261cb
Blagden1786 <georgeablagden@gmail.com>
2025-02-15 22:17:23 +0000

    Not another one

--- environment/main.py
+++ environment/main.py
@@ -38,9 +38,9 @@ def batch_run(seed):
     rand.seed(seed)
 
     t1 = time()
-    run_test_aggregation(ejrpc_one_test, [max, min, np.mean, np.median], './environment/datasets_extended', './environment/plots_batch', True, False)
-    run_test_resources(runtime_test, 10, './environment/datasets_resources', './environment/plots_batch', True, False)
-    run_test_resources(exclusion_test, 10, './environment/datasets_resources', './environment/plots_batch', True, False)
+    #run_test_aggregation(ejrpc_one_test, [max, min, np.mean, np.median], './environment/datasets_extended', './environment/plots_batch', True, False)
+    #run_test_resources(runtime_test, 10, './environment/datasets_resources', './environment/plots_batch', True, False)
+    #run_test_resources(exclusion_test, 10, './environment/datasets_resources', './environment/plots_batch', True, False)
     run_test_resources(ejrpc_one_test, 10, './environment/datasets_resources', './environment/plots_batch', True, False)
     run_test_resources(ejrpa_one_test, 10, './environment/datasets_resources', './environment/plots_batch', True, False)
     t2 = time()
